06f51e2a8ece3758d45324b6da6add7f67675d24
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-07 16:09:31 -0500

    Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,7 +7,6 @@ import requests
 
 if __name__ == '__main__':
     URL = 'https://jsonplaceholder.typicode.com'
-
     id = argv[1]
     rp = \
         requests.get(
@@ -23,8 +22,10 @@ if rp.status_code == 200:
     len_data = len(data)
 
     first = f'Employee {data_name} is done with tasks'
+
     print('{}({}/{}):'.format(first, len_task, len_data))
     for task in fn_task:
         print(f'\t{task["title"]}')
+
 else:
     print('Error: {}'.format(rp.status_code))
