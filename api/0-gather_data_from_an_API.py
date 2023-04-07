fba18a00724e0a51bbf0411d33b8f634a8c8aeee
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-07 15:24:18 -0500

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    URL = 'https://jsonplaceholder.typicode.com'
+
+    id = argv[1]
+    rp = \
+        requests.get(
+            f'{URL}/users/{id}/todos',
+            params={'_expand': 'user'}
+        )
+    
+if rp.status_code == 200:
+    data = rp.json()
+    data_name = data[0]['user']['name']
+    fn_task = [task for task in data if task['completed']]
+    len_task = len(fn_task)
+    len_data = len(data)
+
+    first = f'Employee {data_name} is done with tasks'
+    print('{}({}/{}):'.format(first, len_task, len_data))
+    for task in fn_task:
+        print(f'\t{task["title"]}')
+else:
+    print('Erro: {}'.format(rp.status_code))
