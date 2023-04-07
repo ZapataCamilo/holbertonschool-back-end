1e72deabf36bc8a696424ff2d0915d2c11560d1e
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-07 16:18:45 -0500

    Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,8 +2,8 @@
 """
 returns information about his/her TODO list progress
 """
-from sys import argv
 import requests
+from sys import argv
 
 if __name__ == '__main__':
     URL = 'https://jsonplaceholder.typicode.com'
