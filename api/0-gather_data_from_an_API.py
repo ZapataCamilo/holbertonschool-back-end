2a6bb4899528207b9bed8ab96aa32087da8011a7
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-08 10:29:18 -0500

    Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,25 +7,24 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com'
+    site_URL = 'https://jsonplaceholder.typicode.com'
+
     user_id = argv[1]
     response = requests.get(
-        f'{url}/users/{user_id}/todos',
+        f'{site_URL}/users/{user_id}/todos',
         params={'_expand': 'user'}
     )
 
     if response.status_code == 200:
         data = response.json()
         name = data[0]['user']['name']
-        tasks_ok = [task for task in data if task['completed']]
-        n_task_ok = len(tasks_ok)
-        total_task = len(data)
+        fn_task = [task for task in data if task['completed']]
+        len_task = len(fn_task)
+        len_task_total = len(data)
 
-        first_str = f"Employee {name} is done with tasks"
-
-        print(f"{first_str}({n_task_ok}/{total_task}):")
-        for task in tasks_ok:
-            print(f"\t {task['title']}")
-
+        str_first = f'Employee {name} is done with tasks'
+        print(f'{str_first}({len_task}/{len_task_total}):')
+        for task in fn_task:
+            print(f'\t{task["title"]}')
     else:
-        print(f"Error: {response.status_code}")
+        print(f'Error: {response.status_code}')
