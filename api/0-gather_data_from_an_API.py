d12ed9d0ebcbe0facfcadf6e13f81335fd5aa1dc
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-08 10:48:02 -0500

    Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -17,12 +17,12 @@ if __name__ == '__main__':
 
     if response.status_code == 200:
         data = response.json()
-        name = data[0]['user']['name']
+        EMPLOYEE_NAME = data[0]['user']['name']
         fn_task = [task for task in data if task['completed']]
         len_task = len(fn_task)
         len_task_total = len(data)
 
-        str_first = f'Employee {name} is done with tasks'
+        str_first = f'Employee {EMPLOYEE_NAME} is done with tasks'
         print(f'{str_first}({len_task}/{len_task_total}):')
         for task in fn_task:
             print(f'\t{task["title"]}')
