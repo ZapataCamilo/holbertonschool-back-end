02d1ec4a1ac1ec546a5c854bbd80204896dc26dd
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-08 10:27:18 -0500

    Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""
-returns information about his/her TODO list progress
-"""
+'''returns information about his/her TODO list progress.'''
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    site_URL = 'https://jsonplaceholder.typicode.com'
-
+    url = 'https://jsonplaceholder.typicode.com'
     user_id = argv[1]
     response = requests.get(
-        f'{site_URL}/users/{user_id}/todos',
+        f'{url}/users/{user_id}/todos',
         params={'_expand': 'user'}
     )
 
     if response.status_code == 200:
         data = response.json()
         name = data[0]['user']['name']
-        fn_task = [task for task in data if task['completed']]
-        len_task = len(fn_task)
-        len_task_total = len(data)
+        tasks_ok = [task for task in data if task['completed']]
+        n_task_ok = len(tasks_ok)
+        total_task = len(data)
 
-        str_first = f'Employee {name} is done with tasks'
-        print(f'{str_first}({len_task}/{len_task_total}):')
-        for task in fn_task:
-            print(f'\t{task["title"]}')
+        first_str = f"Employee {name} is done with tasks"
+
+        print(f"{first_str}({n_task_ok}/{total_task}):")
+        for task in tasks_ok:
+            print(f"\t {task['title']}")
+
     else:
-        print(f'Error: {response.status_code}')
+        print(f"Error: {response.status_code}")
